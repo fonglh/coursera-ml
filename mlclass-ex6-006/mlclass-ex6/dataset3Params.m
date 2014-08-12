3a4127ba14328e6e107d082d078bc6b496555ef7
fonglh <flh@server.com>
2014-08-12 15:21:42 +0800

    Finish task 2. Compute best C and sigma using cross val data.

--- mlclass-ex6-006/mlclass-ex6/dataset3Params.m
+++ mlclass-ex6-006/mlclass-ex6/dataset3Params.m
@@ -23,11 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values = [0.01 0.03 0.1 0.3 1 3 10 30];
+predict_error_size = size(values, 2);
+predict_error = zeros(predict_error_size);
 
+for i=1:size(values, 2);
+  for j=1:size(values, 2);
+    model = svmTrain(X, y, values(i), @(x1, x2) gaussianKernel(x1, x2, values(j)));
+    predictions = svmPredict(model, Xval);
+    predict_error(i, j) = mean(double(predictions ~= yval));
+  end
+end
 
+% gets the minimum in each column as a row vector
+% row_index should contain the row numbers of the min element in each column
+[col_min, row_index] = min(predict_error);
+% gets the minimum in the row vector
+% col_index indicates which column has the smallest item
+[min_error, col_index] = min(col_min);
 
+% row_index(col_index) is the row number of the smallest item
+% col_index is the column number of the smallest item
 
-
+C = values(row_index(col_index));
+sigma = values(col_index);
 
 % =========================================================================
 
